43af750b5b3c8b9b54a577bd2ad5ae523cae2961
Michael <104609759+mollmikey@users.noreply.github.com>
2024-03-21 08:14:05 +1300

    fix the double list display

--- client/components/AppLayout.tsx
+++ client/components/AppLayout.tsx
@@ -1,13 +1,9 @@
 import { Outlet } from 'react-router-dom'
 
-import { fetchPokemonGeneration } from '../apis/pokemon.ts'
-import PokemonList from './PokemonList'
-
 export default function AppLayout() {
   return (
     <>
       <h1>Query &apos;em All 🐭⚡</h1>
-      <PokemonList />
       <Outlet />
     </>
   )
--- client/components/GenerationList.tsx
+++
@@ -1,9 +0,0 @@
-import { useQuery } from '@tanstack/react-query'
-import LoadingSpinner from './LoadingSpinner.tsx'
-import { Link } from 'react-router-dom'
-
-function GenerationList() {
-  return()
-}
-
-export default GenerationList
